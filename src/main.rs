64a953a4922c6ee351786313da34f8530e7b6cd5
bijeshos <bijesh.os@gmail.com>
2023-02-27 14:20:23 +0530

    refactoring: moved arg related code to another module

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,6 @@
-use std::env;
-use std::process;
-
-use learning_rust::Config;
-
 fn main() {
     println!("starting program");
-    let args: Vec<String> = env::args().collect();
-    /* if args.len()<3{
-         println!("not enough arguments. stopping program");
-         process::exit(1)
-     }*/
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("error parsing arguments: {}", err);
-        process::exit(1)
-    });
-    //dbg!(args);
-
-    if let Err(e) = learning_rust::run(config) {
-        println!("application error: {e}");
-        process::exit(1)
-    }
+    learning_rust::run_examples()
 }
 
 
