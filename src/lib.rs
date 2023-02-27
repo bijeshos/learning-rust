2547c3322466770de3c9bf8a52e089800fc06e1f
bijeshos <bijesh.os@gmail.com>
2023-02-27 13:57:27 +0530

    minor tweaks

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,48 @@
+use std::env::var;
 use std::error::Error;
 use std::io;
+
+mod variables;
+mod data_types;
+mod functions;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    println!("input arg : query: {}", config.query);
+    println!("input arg : file path: {}", config.file_path);
+    //read_command_line_input();
+    variables::basic_variables();
+    data_types::scalar_data_types();
+    data_types::compound_data_types();
+    //functions:private_function(); // this won't compile
+    functions::function_examples();
+
+    Ok(())
+}
+
+
+/*#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+            Rust.
+            safe, fast, productive.
+            pick three.
+            Duct tape.
+        ";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+}*/
+
+/*pub fn search<'a>(query: &str, contents: &str) -> Vec<&'a str> {
+    vec![];
+
+    print!("query:{}", query);
+    print!("contents:{}",contents);
+}*/
 
 pub struct Config {
     query: String,
@@ -9,7 +52,7 @@ pub struct Config {
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            return Err("not enough arguments");
+            return Err("not enough arguments; expecting query and file_path as arguments");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
@@ -18,12 +61,6 @@ impl Config {
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("arg: query: {}", config.query);
-    println!("arg: file path: {}", config.file_path);
-    read_command_line_input();
-    Ok(())
-}
 
 
 fn read_command_line_input() {
