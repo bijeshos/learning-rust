e5cdcff6d535f6a1594a302e66e2aea4f7b201cf
bijeshos <bijesh.os@gmail.com>
2023-02-27 14:32:12 +0530

    variables refactoring

--- src/variables.rs
+++ src/variables.rs
@@ -1,26 +1,12 @@
 pub fn variable_examples() {
     println!("-----------");
+    immutable_variable();
+    mutable_variable();
+    constant();
+    shadowing();
+}
 
-    //immutable variable (variable are immutable by default)
-    let x = 5;
-    println!("value of x is: {}", x);
-
-    //below line won't compile since x is immutable
-    //x = 6;
-    println!("-----------");
-
-    //mutable variable
-    let mut y = 10;
-    println!("value of y (after initialization) is: {}", y);
-    y = 11;
-    println!("value of y (after re-assignment) is: {}", y);
-    println!("-----------");
-
-    //constants : always immutable
-    const PI: f32 = 3.141;
-    println!("const PI : {}", PI);
-
-    println!("-----------");
+fn shadowing() {
     //shadowing
     //first declaration
     let x = 6;
@@ -30,5 +16,31 @@ pub fn variable_examples() {
         let x = 7.0;
         println!("value of x(after shadowing) : {:?}", x);
     }
+    println!("-----------");
+}
+
+fn mutable_variable() {
+    //mutable variable
+    let mut y = 10;
+    println!("value of y (after initialization) is: {}", y);
+    y = 11;
+    println!("value of y (after re-assignment) is: {}", y);
+    println!("-----------");
+}
+
+fn immutable_variable() {
+    //immutable variable (variable are immutable by default)
+    let x = 5;
+    println!("value of x is: {}", x);
+
+    //below line won't compile since x is immutable
+    //x = 6;
+    println!("-----------");
+}
+
+fn constant(){
+    //constants : always immutable
+    const PI: f32 = 3.141;
+    println!("const PI : {}", PI);
     println!("-----------");
 }
