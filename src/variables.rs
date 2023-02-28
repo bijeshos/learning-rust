3d768e063cc704b73f47780c44e1b6a049b64e29
bijeshos <bijesh.os@gmail.com>
2023-02-28 10:03:01 +0530

    updated println messages

--- src/variables.rs
+++ src/variables.rs
@@ -1,12 +1,35 @@
 pub fn run_variable_examples() {
-    println!("-----------");
+    println!("----------- variable examples : begin -----------\n");
     immutable_variable();
     mutable_variable();
     constant();
     shadowing();
+    println!("----------- variable examples : end -----------\n");
+}
+
+fn mutable_variable() {
+    println!("**  mutable variable example : begin");
+    //mutable variable
+    let mut y = 10;
+    println!("value of y (after initialization) is: {}", y);
+    y = 11;
+    println!("value of y (after re-assignment) is: {}", y);
+    println!("**  mutable variable example : end \n");
+}
+
+fn immutable_variable() {
+    println!("** immutable variable example : begin");
+    //immutable variable (variable are immutable by default)
+    let x = 5;
+    println!("value of x is: {}", x);
+
+    //below line won't compile since x is immutable
+    //x = 6;
+    println!("** immutable variable example : end\n");
 }
 
 fn shadowing() {
+    println!("** shadowing example : begin");
     //shadowing
     //first declaration
     let x = 6;
@@ -16,31 +39,13 @@ fn shadowing() {
         let x = 7.0;
         println!("value of x(after shadowing) : {:?}", x);
     }
-    println!("-----------");
+    println!("** shadowing example : end \n");
 }
 
-fn mutable_variable() {
-    //mutable variable
-    let mut y = 10;
-    println!("value of y (after initialization) is: {}", y);
-    y = 11;
-    println!("value of y (after re-assignment) is: {}", y);
-    println!("-----------");
-}
-
-fn immutable_variable() {
-    //immutable variable (variable are immutable by default)
-    let x = 5;
-    println!("value of x is: {}", x);
-
-    //below line won't compile since x is immutable
-    //x = 6;
-    println!("-----------");
-}
-
-fn constant(){
+fn constant() {
+    println!("** constant example : begin");
     //constants : always immutable
     const PI: f32 = 3.141;
     println!("const PI : {}", PI);
-    println!("-----------");
+    println!("** constant example : end\n");
 }
