cc9820f9e80e4e6c81037a403b5a9af1ded0a748
bijeshos <bijesh.os@gmail.com>
2023-02-28 10:03:45 +0530

    updated println messages

--- src/data_types.rs
+++ src/data_types.rs
@@ -1,32 +1,42 @@
 pub fn run_scalar_data_type_examples() {
-    println!("-----------");
+    println!("----------- scalar data type examples : begin -----------\n");
     unsigned_integer();
     signed_integer();
     float();
     boolean();
     character();
+    println!("----------- scalar data type examples : end -----------\n");
+}
+
+pub fn run_compound_data_type_examples() {
+    println!("----------- compound data type examples : begin -----------\n");
+    tuple_example();
+    array_example();
+    println!("----------- compound data type examples : end -----------\n");
 }
 
 fn character() {
+    println!("**  character example : begin");
     //character types : single quotes, four bytes in size, represent unicode scalar value
     let ch: char = 'a';
 
     println!("char : {}", ch);
-    println!("-----------");
+    println!("**  character example : end\n");
 }
 
 fn boolean() {
+    println!("**  boolean example : begin");
     // boolean types
     let bool_false: bool = false;
     let bool_true: bool = true;
 
     println!("bool_false : {}", bool_false);
     println!("bool_true : {}", bool_true);
-    println!("-----------");
+    println!("**  boolean example : end\n");
 }
 
 fn float() {
-    //float
+    println!("**  float example : begin");
     //single precision
     let float_f32: f32 = 3.0;
 
@@ -35,10 +45,12 @@ fn float() {
 
     println!("float_f32 : {}", float_f32);
     println!("float_f64 : {}", float_f64);
-    println!("-----------");
+    println!("**  float example : end\n");
+
 }
 
 fn signed_integer() {
+    println!("**  signed integer example : begin");
     // integers : signed
     let signed_int_8bit: u8 = 31;
     let signed_int_16bit: u16 = 31;
@@ -54,10 +66,11 @@ fn signed_integer() {
     println!("signed_int_64bit : {}", signed_int_64bit);
     println!("signed_int_128bit : {}", signed_int_128bit);
     println!("signed_int_arch : {}", signed_int_arch);
-    println!("-----------");
+    println!("**  signed integer example : end\n");
 }
 
 fn unsigned_integer() {
+    println!("**  unsigned integer example : begin");
     // integers : unsigned
     let unsigned_int_8bit: i8 = 31;
     let unsigned_int_16bit: i16 = 31;
@@ -73,11 +86,13 @@ fn unsigned_integer() {
     println!("unsigned_int_64bit : {}", unsigned_int_64bit);
     println!("unsigned_int_128bit : {}", unsigned_int_128bit);
     println!("unsigned_int_arch : {}", unsigned_int_arch);
-    println!("-----------");
+    println!("** unsigned integer example : end\n");
 }
 
-pub fn run_compound_data_type_examples() {
-    println!("-----------");
+
+
+fn tuple_example() {
+    println!("**  tuple example : begin");
     //tuples : group of values with different data types; has fixed length
     let tup1: (i32, f64, u8) = (500, 6.8, 8);
     let tup2 = (501, 7.8, 9);
@@ -101,9 +116,11 @@ pub fn run_compound_data_type_examples() {
     println!("x : {}, y: {}, z: {}", x, y, z);
 
     println!("k (unit) : {:?}", k);
-    println!("-----------");
+    println!("**  tuple example : end\n");
+}
 
-
+fn array_example() {
+    println!("**  array example : begin");
     //array : group of values with same data types; has fixed length; data is allocated to stack; not heap
     let arr1 = [0, 2, 3, 4, 5];
     let arr2: [i32; 5] = [6, 7, 8, 9, 10];
@@ -117,5 +134,5 @@ pub fn run_compound_data_type_examples() {
     println!("arr3 : {:?}", arr3);
     println!("q1_months : {:?}", q1_months);
     println!("second month : {}", q1_months[1]);
-    println!("-----------");
+    println!("**  array example : end\n");
 }
