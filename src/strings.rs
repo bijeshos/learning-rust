dd94f9ab6cedbedcb0db8f5e367ea4ddef11b955
bijeshos <bijesh.os@gmail.com>
2023-03-23 15:56:39 +0530

    formatting changes

--- src/strings.rs
+++ src/strings.rs
@@ -28,34 +28,34 @@ fn declaration_example() {
     //creates a String from string literal
     let s5 = String::from("this is test sentence 3");
 
-    println!("s1:{}",s1);
-    println!("s2:{}",s2);
-    println!("s3:{}",s3);
-    println!("s4:{}",s4);
-    println!("s5:{}",s5);
+    println!("s1:{}", s1);
+    println!("s2:{}", s2);
+    println!("s3:{}", s3);
+    println!("s4:{}", s4);
+    println!("s5:{}", s5);
 
     log::end_example("string: declaration");
 }
 
-fn updating_string(){
+fn updating_string() {
     log::start_example("string: update");
     //using push_str : append a string slice
     let mut s1 = String::from("hi");
-    println!("s1 (before push_str) : {}",s1);
+    println!("s1 (before push_str) : {}", s1);
     s1.push_str(" there");
-    println!("s1 (after push_str) : {}",s1);
+    println!("s1 (after push_str) : {}", s1);
 
     //using push - append one char
     let mut s2 = String::from("hello");
-    println!("s2 (before push) : {}",s2);
+    println!("s2 (before push) : {}", s2);
     s2.push('x');
-    println!("s2 (before push) : {}",s2);
+    println!("s2 (before push) : {}", s2);
 
     //using + operator
-    let  s3 = String::from("hello");
-    let  s4 = String::from(" there");
+    let s3 = String::from("hello");
+    let s4 = String::from(" there");
     let s5 = s3 + &s4;
-    println!("s5 (after +) : {}",s5);
+    println!("s5 (after +) : {}", s5);
     //note: s3 would no longer valid now, since it's ownership has been changed
     //reference to s4 was passed; not s4 itself
     // above statement takes ownership of s3, appends copy of contents to it and returns the ownership to the result
@@ -64,33 +64,33 @@ fn updating_string(){
 
 
     //using format! macro
-    let  s6 = String::from("one");
-    let  s7 = String::from("two");
-    let  s8 = String::from("three");
+    let s6 = String::from("one");
+    let s7 = String::from("two");
+    let s8 = String::from("three");
     let s9 = format!("{s6}-{s7}-{s8}"); //this does not take ownership of args
-    println!("s9 (after format) : {}",s9);
+    println!("s9 (after format) : {}", s9);
 
     log::end_example("string: update");
 }
 
-fn slicing_strings(){
+fn slicing_strings() {
     log::begin_topic("string: slice");
     let s1 = String::from("hello there");
     let s2 = &s1[0..4];  //gets first 4 bytes (not chars) of s1
-    println!("s2: {}",s2);
+    println!("s2: {}", s2);
     log::end_example("string: slice");
 }
 
-fn string_iteration(){
+fn string_iteration() {
     log::begin_topic("string: iteration");
     let s1 = String::from("hello there");
 
-    for c in s1.chars(){
-        println!("c: {}",c);
+    for c in s1.chars() {
+        println!("c: {}", c);
     }
 
-    for b in s1.bytes(){
-        println!("b: {}",b);
+    for b in s1.bytes() {
+        println!("b: {}", b);
     }
 
     log::end_example("string: iteration");
