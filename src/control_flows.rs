6de250539bd2af2137e02f3927cf5c655a3aac2c
bijeshos <bijesh.os@gmail.com>
2023-03-23 15:54:32 +0530

    formatting changes

--- src/control_flows.rs
+++ src/control_flows.rs
@@ -105,9 +105,9 @@ fn while_example() {
 fn for_example() {
     log::start_example("for");
 
-    let a = [1,2,3,4,5];
-    for element in a{
-        println!("value in a is : {}",element);
+    let a = [1, 2, 3, 4, 5];
+    for element in a {
+        println!("value in a is : {}", element);
     }
 
     /*for number in (1..4).rev() {
