1a9b2774ac21a99bd3cf9e2719893f0628b6ce0a
bijeshos <bijesh.os@gmail.com>
2024-02-05 14:28:49 +0530

    formatting changes

--- src/sub_module_a.rs
+++ src/sub_module_a.rs
@@ -1,7 +1,6 @@
 use crate::log;
 
-pub mod sub_module_aa;
-//declaring as pub sub module; definition is inside sub_module_a directory
+pub mod sub_module_aa; //declaring as pub sub module; definition is inside sub_module_a directory
 mod sub_module_ab; //declaring as private sub module; definition is inside sub_module_a directory
 
 pub fn run_sub_module_a_examples() {
